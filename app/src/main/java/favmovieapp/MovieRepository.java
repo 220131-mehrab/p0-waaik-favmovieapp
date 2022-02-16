1805bb56dedad9b7ff3bdb92347c5f8db6973644
Waaik Perrin <keythageniuz@Waaiks-MacBook-Pro-2.local>
2022-02-16 09:47:50 -0500

    Start of FavMovieApp 1.1

--- app/src/main/java/favmovieapp/MovieRepository.java
+++ app/src/main/java/favmovieapp/MovieRepository.java
@@ -1,4 +1,25 @@
 package favmovieapp;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieRepository {
+    private List<String> movies; //Setup data structure
+    private File file; //File variable
+    public MovieRepository(String filename) { //Construct fields of data structure
+        this.movies = new ArrayList<>();
+        this.file = new File("" + filename);
+        load(); //load file
+    }
+    //setup Input stream method of API data
+    private void InputStream() {
+    }
+    //load data into repository method
+    private void load(){
+
+
+            }
+        }
+    }
 }
