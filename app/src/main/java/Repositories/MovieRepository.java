69c803b546dd52404c6143fb9c2ba46997ddddfb
Waaik Perrin <keythageniuz@Waaiks-MacBook-Pro-2.local>
2022-02-21 12:02:12 -0500

    Updated MovieRepository.java to parse data into JsonNode Tree

--- app/src/main/java/Repositories/MovieRepository.java
+++ app/src/main/java/Repositories/MovieRepository.java
@@ -7,9 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import domains.Movies;
+
 import org.w3c.dom.ls.LSOutput;
 
 
@@ -21,16 +23,22 @@ public class MovieRepository {
                 //Collecting values for Movies Objects
                 ObjectMapper mapper = new ObjectMapper();
                 String movieJson = "{\"adult\":false,\"backdrop_path\":null,\"genre_ids\":[18,9648],\"id\":582585,\"original_language\":\"ja\",\"original_title\":\"\\u3044\\u306e\\u3061\\u306e\\u6d77\",\"overview\":\"\",\"poster_path\":\"\\/3zaXzsCBDkYmKRAFYNR5WuWsTcl.jpg\",\"release_date\":\"2001-04-07\",\"title\":\"Closed Ward\",\"video\":false,\"vote_average\":0.0,\"vote_count\":0,\"popularity\":0.6,\"id_imdb\":\"tt0263470\"}";
-                //Deserialization of movieJson just created and import into the object class Movies
-                Movies movies = mapper.readValue(movieJson, Movies.class);
+                //parses data into a data tree collection
+                JsonNode jsonNode = mapper.readTree(movieJson);
+                //pulls out specific data you want from node string data tree
+                String movieTitle = String.valueOf(jsonNode.get("title"));
 
+                System.out.println(movieTitle);
 
                 //turn data into a String
-                movies.toString();
-                
+                //movies.toString();
+
+
 
 
             } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
--- app/src/main/java/domains/Movies.java
+++ app/src/main/java/domains/Movies.java
@@ -2,6 +2,13 @@ package domains;
 
 public class Movies {
     //creating Movie variables
+    private String adult;
+    private String backdrop_path;
+    private String id;
+    private String title;
+    private String vote_average;
+    private String vote_count;
+    private String popularity;
     private String genre_ids;
     private String original_language;
     public String original_title;
@@ -14,8 +21,19 @@ public class Movies {
     public Movies(){
         super();
     }
+
+
+
+
     //initializing Movies variables
-    public Movies(String genre_ids, String original_language, String original_title, String overview, String poster_path, String release_date, String video, String id_imbd) {
+    public Movies(String adult, String backdrop_path, String id, String title, String vote_average, String vote_count, String popularity, String genre_ids, String original_language, String original_title, String overview, String poster_path, String release_date, String video, String id_imbd) {
+        this.adult = adult;
+        this.backdrop_path = backdrop_path;
+        this.id = id;
+        this.title = title;
+        this.vote_average = vote_average;
+        this.vote_count = vote_count;
+        this.popularity = popularity;
         this.genre_ids = genre_ids;
         this.original_language = original_language;
         this.original_title = original_title;
@@ -26,6 +44,20 @@ public class Movies {
         this.id_imbd = id_imbd;
     }
     //setting up get methods for variables to save values
+    public String getAdult() {return adult;}
+
+    public String getBackdrop_path() {return backdrop_path;}
+
+    public String getId() {return id;}
+
+    public String getTitle() {return title;}
+
+    public String getVote_average() {return vote_average;}
+
+    public String getVote_count() {return vote_count;}
+
+    public String getPopularity() {return popularity;}
+
     public String getGenre_ids() {
         return genre_ids;
     }
